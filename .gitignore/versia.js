35c27a3263ce461036766a74f87b2e61e85af43d
Nefheure <43937013+Nefheure@users.noreply.github.com>
2018-11-23 22:23:22 +0100

    Update versia.js

--- .gitignore/versia.js
+++ .gitignore/versia.js
@@ -5,8 +5,6 @@ const prefix = "v.";
 
 const ms = require('ms');
 
-const ytdl = require('ytdl-core');
-
 var bot = new Discord.Client();
   
 let statuses = ['v.help', 'Ready !', 'Versia'];
@@ -275,19 +273,5 @@ bot.on('message', message => {
         message.channel.send(embed)
     }
 });
-
-bot.on('message', message => {
-
-    if (!message.member.voiceChannel) return message.channel.send("Please connect to a Voice Channel");
-    if (message.guild.me.voiceChannel) return message.channel.send("I'm not ready to connect to the voice channel");
-    if (!args[0]) return message.channel.send("please kindly give me a link");
-    let validate = await ytdl.validateURL(args[0]);
-    if (!validate) return message.channel.send("please kindly give me a **valid** link");
-    let info = await ytdl.getInfo(args[0]);
-    let connection = await message.member.voiceChannel.join();
-    let dispatcher = await connection.play(ytdl(args[0], { filter: 'audioonly'}));
-    message.channel.send(`**Now playing**: ${info.title}`);
-
-}):
 bot.login(process.env.TOKEN);
 
